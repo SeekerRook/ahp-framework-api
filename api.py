43837489c4a325334f294d33cfd6a4a5acf9c04e
SeekerRook <xrist.vardakis@gmail.com>
2026-07-03 11:04:03 +0300

    modified for KPI ids + vikor for tests

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ import os
 import requests
 import time
 import logging
+import numpy as np
+from pymcdm.methods import VIKOR
 
 logger = logging.getLogger('uvicorn.error')
 logger.setLevel(logging.DEBUG)
@@ -23,19 +25,67 @@ def ahp(alternatives,preferences):
     selection =  res.json()       
     #logger.debug(selection) 
     logger.debug(f"ahp_time {time.time()-start}")
-    try:
-        return {"MaterialID" : selection["best_alternative"]["materialID"],"supplierID" : selection["best_alternative"]["supplierID"]}
-    except:
-        return {"RESULT":"ERROR","PAYLOAD":payload,"RESPONSE":selection}
+    return selection
 
 def run_ahp(alternatives,materials,preferences=[]):
     res = {}
     for i,material in enumerate(materials):
+        selection = ahp(alternatives[material],preferences[i])
+
         try:
             #selection = lternatives[material][ahp(alternatives[material],preferences[i])]
-            res[material] = ahp(alternatives[material],preferences[i])
+            res[material] = {"MaterialID" : selection["best_alternative"]["materialID"],"supplierID" : selection["best_alternative"]["supplierID"]}
         except:
-            return ahp(alternatives[material],preferences[i])
+            return {"RESULT":"ERROR","RESPONSE":selection}
+
+    return res
+def run_ahp_full(alternatives,materials,preferences=[]):
+    res = {}
+    for i,material in enumerate(materials):
+        selection = ahp(alternatives[material],preferences[i])
+
+
+        res[material] = selection#{"MaterialID" : selection["best_alternative"]["materialID"],"supplierID" : selection["best_alternative"]["supplierID"]}
+
+
+    return res
+def vikor(matrix,
+        weights,
+        types,
+
+        v=0.5):
+
+    body = VIKOR()
+
+
+    res = body(matrix,weights,types)
+
+    return res
+def make_DM(alternatives):
+    result = []
+    for alt in alternatives:
+        r = [alt[f"C{i+1}"] for i in range(7)]
+        result.append(r)
+    return np.array(result)
+def weight_transform(w,kpi_w=[0.55,0.45,1,0.6,0.4,0.5,0.5]):
+
+    return [w[0]*kpi_w[0],w[0]*kpi_w[1],w[1]*kpi_w[2],w[2]*kpi_w[3],w[2]*kpi_w[4],w[3]*kpi_w[5],w[3]*kpi_w[6]]
+
+
+def run_vikor(alternatives,materials,preferences=[]):
+    start = time.time()
+
+    res = {}
+    for i,material in enumerate(materials):
+        dm = make_DM(alternatives=alternatives[material])
+        w = weight_transform(preferences[i])
+        selection = vikor(dm,w,types=[1,1,-1,-1,1,1,1])
+
+
+        res[material] = int(np.argmax(selection))#{"MaterialID" : selection["best_alternative"]["materialID"],"supplierID" : selection["best_alternative"]["supplierID"]}
+
+    logger.debug(f"vikor_time {time.time()-start}")
+
     return res
 from fastapi import FastAPI, HTTPException,Body
 
@@ -121,6 +171,58 @@ async def manage_order(order: dict = Body(...)):
         return selection
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/ahp/simple", summary="Execute order, with preferences as natural language descriptors")
+
+async def manage_order(order: dict = Body(...)):
+    try:
+        logger.debug(f"Order-Simple")
+        start = time.time()
+        intent_map = {
+            "balanced":[0.25,0.25,0.25,0.25],
+            "cost":[0.1,0.1,0.7,0.1],
+            "quality":[0.7,0.1,0.1,0.1],
+            "delivery":[0.1,0.7,0.1,0.1],
+            "environment":[0.1,0.1,0.1,0.7],
+            "recommended":[0.35,0.15,0.35,0.15]
+        }
+        materials = order.get("materials", [])
+        preferences = [intent_map[i] for i in order.get("preferences")]
+        data = fetch_data()
+           
+        selection = run_ahp_full(data,materials,preferences)
+        logger.debug(f"Total(AHP)  {time.time()-start}")
+        
+        return selection
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/vikor/simple", summary="Execute order, with preferences as natural language descriptors")
+
+async def manage_order(order: dict = Body(...)):
+    try:
+        logger.debug(f"Order-Simple")
+        start = time.time()
+        intent_map = {
+            "balanced":[0.25,0.25,0.25,0.25],
+            "cost":[0.1,0.1,0.7,0.1],
+            "quality":[0.7,0.1,0.1,0.1],
+            "delivery":[0.1,0.7,0.1,0.1],
+            "environment":[0.1,0.1,0.1,0.7],
+            "recommended":[0.35,0.15,0.35,0.15]
+        }
+        materials = order.get("materials", [])
+        preferences = [intent_map[i] for i in order.get("preferences")]
+        data = fetch_data()
+           
+        selection = run_vikor(data,materials,preferences)
+        logger.debug(f"Total(VIKOR)  {time.time()-start}")
+        
+        return selection
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 
 
 
